bc9d0f3aa463aea6e2d7bbe77684a40212aef15e
anitaprova <104943181+anitaprova@users.noreply.github.com>
2025-03-02 18:26:08 -0500

    added more pages and components

--- Diava/src/App.jsx
+++ Diava/src/App.jsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import Book from "./pages/BookView.jsx";
+import Home from "./pages/Home.jsx"
+import Navbar from "./components/Navbar.jsx"
 import "./index.css"
 
 function App() {
   return (
     <>
+    <Navbar></Navbar>
       <Routes>
-        <Route path="/" element={<Book />} />
+        <Route path="/" element={<Home />} />
         <Route path="/Book" element={<Book />} />
       </Routes>
     </>
