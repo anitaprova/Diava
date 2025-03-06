23b57eb34f3c55ffd4c9f6b7d3d0660b3514895a
anitaprova <104943181+anitaprova@users.noreply.github.com>
2025-03-06 15:40:55 -0500

    added autocomplete for search

--- Diava/src/components/Navbar.jsx
+++ Diava/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
@@ -7,10 +7,17 @@ import SearchIcon from "@mui/icons-material/Search";
 import { InputBase, IconButton } from "@mui/material";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
+import { debounce } from "@mui/material/utils";
+import Autocomplete from "@mui/material/Autocomplete";
+import axios from "axios";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
 
 export default function Navbar() {
   const API_KEY = import.meta.env.VITE_GOOGLE_BOOKS_API_KEY;
   const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
   const navigate = useNavigate();
 
   const searchBook = (event) => {
@@ -19,6 +26,26 @@ export default function Navbar() {
     }
   };
 
+  const searchDelayed = debounce((query) => {
+    if (query.trim()) {
+      axios
+        .get(
+          `https://www.googleapis.com/books/v1/volumes?q=${query}&key=${API_KEY}`
+        )
+        .then((response) => setResults(response.data.items || []))
+        .catch((error) => console.error("Error fetching books:", error));
+    } else {
+      setResults([]);
+    }
+  }, 1000);
+
+
+  useEffect(() => {
+    searchDelayed(query);
+  }, [query]);
+
+  console.log(results);
+
   return (
     <AppBar position="static">
       <Toolbar className="flex justify-between">
@@ -26,13 +53,62 @@ export default function Navbar() {
 
         <div className="bg-sand flex items-center flex-grow mx-30 rounded-2xl px-3">
           <SearchIcon className="mr-2" />
-          <InputBase
+          <Stack className="outline-none w-full text-lg text-grey">
+            <Autocomplete
+              sx={{
+                width: "full",
+                borderRadius: "50px",
+                "& .MuiInputBase-root": {
+                  padding: "0px",
+                },
+                "& .MuiOutlinedInput-notchedOutline": {
+                  borderColor: "transparent",
+                },
+                "&:hover .MuiOutlinedInput-notchedOutline": {
+                  borderColor: "transparent",
+                },
+              }}
+              freeSolo
+              getOptionLabel={(result) => `${result.volumeInfo?.title}`}
+              options={results}
+              onInputChange={(event, newValue) => setQuery(newValue)}
+              onKeyDown={searchBook}
+              onChange={(event, selectedBook) => {
+                if (selectedBook) {
+                  navigate(`/book/${selectedBook.id}`);
+                }
+              }}
+              renderInput={(params) => <TextField {...params} />}
+              renderOption={(props, result) => (
+                <Box
+                  component="li"
+                  onChange={() => navigate(`/book/${result.id}`)}
+                  {...props}
+                >
+                  <img
+                    className="w-10 mr-5"
+                    src={result.volumeInfo?.imageLinks.smallThumbnail}
+                  />
+                  <div className="flex items-center gap-2">
+                    <span className="font-medium bold">
+                      {result.volumeInfo?.title}
+                    </span>
+                    <span className="text-sm text-grey">
+                      by {result.volumeInfo?.authors?.join(", ")}
+                    </span>
+                  </div>
+                </Box>
+              )}
+            />
+          </Stack>
+
+          {/* <InputBase
             placeholder="Search..."
             className="outline-none w-full text-lg text-grey"
             value={query}
             onChange={(e) => setQuery(e.target.value)}
             onKeyDown={searchBook}
-          />
+          /> */}
         </div>
 
         <div className="flex gap-x-6">
