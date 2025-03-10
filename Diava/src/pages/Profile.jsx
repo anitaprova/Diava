71fcb5da387444d25613b682efa0b0c4a8250740
anitaprova <104943181+anitaprova@users.noreply.github.com>
2025-03-09 21:00:23 -0400

    created notebook card

--- Diava/src/pages/Profile.jsx
+++ Diava/src/pages/Profile.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import { Box, Button, Typography, Chip } from "@mui/material";
+import { Box, Button, Typography, Chip, Paper } from "@mui/material";
 import { Whatshot as Streak } from "@mui/icons-material";
+import TrendingUpIcon from "@mui/icons-material/TrendingUp";
+import TrackChangesIcon from "@mui/icons-material/TrackChanges";
 
 export default function Profile() {
   return (
@@ -44,7 +46,56 @@ export default function Profile() {
         </Box>
       </Box>
       <div className="bg-white mb-5"></div>
-      <div className="bg-darkbrown"></div>
+      <div className="bg-darkbrown grid grid-cols-2">
+        <Paper
+          className="max-w-md mx-auto rounded-md overflow-hidden shadow-lg"
+          sx={{
+            backgroundColor: "#c1a882",
+            color: "#433422",
+            position: "relative",
+            pt: 1,
+          }}
+        >
+          <div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-16 h-5 bg-white/80 rounded-b-sm" />
+
+          <div className="flex justify-between px-4 mb-2">
+            {Array(6)
+              .fill(0)
+              .map((_, i) => (
+                <div key={i} className="w-7 h-7 rounded-full bg-darkbrown" />
+              ))}
+          </div>
+
+          <Typography variant="h5" className="text-center mb-2 font-semibold">
+            Reading Progress
+          </Typography>
+          <Box className="px-4 py-2 border-t border-gray-600 flex justify-between items-center">
+            <Typography variant="body2" className="font-medium">
+              2025 Reading Challenge
+            </Typography>
+            <Typography variant="body2" className="font-medium">
+              24/50
+            </Typography>
+          </Box>
+          <Box className="px-4 py-2 border-t border-gray-600 flex justify-between items-center">
+            <Typography variant="body2" className="font-medium">
+              <TrendingUpIcon /> Current Pace
+            </Typography>
+            <Typography variant="body2" className="font-medium">
+              <TrackChangesIcon /> Next Milestone
+            </Typography>
+          </Box>
+          <Box className="px-4 py-2 border-t border-gray-600 flex justify-between items-center">
+            <Typography variant="body2" className="font-medium">
+              2 Books Ahead of Schedule
+            </Typography>
+            <Typography variant="body2" className="font-medium">
+              25 Books - Silver Reader
+            </Typography>
+          </Box>
+          <Box className="px-4 py-2 border-t border-gray-600 flex justify-between items-center"></Box>
+        </Paper>
+      </div>
     </Box>
   );
 }
