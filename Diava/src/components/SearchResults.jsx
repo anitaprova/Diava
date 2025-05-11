87ef3b5f57add7842da2f1331cbd3563b9e73aea
anitaprova <104943181+anitaprova@users.noreply.github.com>
2025-05-11 19:48:02 -0400

    added search by isbn

--- Diava/src/components/SearchResults.jsx
+++ Diava/src/components/SearchResults.jsx
@@ -1,26 +1,50 @@
-import React, { useEffect, useState } from "react";
-import { useSearchParams } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
+import { useEffect, useState } from "react";
+import { useSearchParams, useNavigate } from "react-router-dom";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import { Typography } from "@mui/material";
+import Typography from "@mui/material/Typography";
 import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import FormControl from "@mui/material/FormControl";
 import FormLabel from "@mui/material/FormLabel";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import axios from "axios";
+import { useRef } from "react";
 
 export default function SearchResults() {
   const API_KEY = import.meta.env.VITE_GOOGLE_BOOKS_API_KEY;
   const [searchParams] = useSearchParams();
-  const query = searchParams.get("q");
+  const [query, setQuery] = useState(searchParams.get("q"));
   const [results, setResults] = useState([]);
+  const [searchType, setSearchType] = useState("Title");
   const navigate = useNavigate();
 
+  const handleSearch = () => {
+    if (searchType == "title" && query) {
+      axios
+        .get(
+          `https://www.googleapis.com/books/v1/volumes?q=${query}&key=${API_KEY}`
+        )
+        .then((response) => setResults(response.data.items || []))
+        .catch((error) => console.error("Error fetching books:", error));
+    }
+    else if (searchType == "isbn" && query) {
+      axios
+        .get(
+          `https://www.googleapis.com/books/v1/volumes?q=isbn:${query}&key=${API_KEY}`
+        )
+        .then((response) => setResults(response.data.items || []))
+        .catch((error) => console.error("Error fetching books:", error));
+    }
+  };
+
+  const gotQuery = useRef(false);
   useEffect(() => {
-    if (query) {
+    if (query && !gotQuery) {
+      gotQuery.current = true;
       axios
         .get(
           `https://www.googleapis.com/books/v1/volumes?q=${query}&key=${API_KEY}`
@@ -30,20 +54,38 @@ export default function SearchResults() {
     }
   }, [query]);
 
+  console.log(query);
+
   return (
-    <div className="flex font-merriweather mr-10 gap-x-20">
-      <div className="bg-sand p-5 pr-20">
+    <div className="grid grid-cols-5 font-merriweather gap-x-10">
+      <div className="col-span-1 bg-sand p-5 h-full">
         <FormControl>
           <FormLabel>
             <p className="font-merriweather text-black text-lg">Search by</p>
           </FormLabel>
-          <RadioGroup defaultValue="title">
+          <div className="flex">
+            <TextField
+              variant="outlined"
+              size="small"
+              defaultValue={query}
+              onChange={(event) => setQuery(event.target.value)}
+            />
+            <Button onClick={handleSearch} variant="dark">
+              Search
+            </Button>
+          </div>
+
+          <RadioGroup
+            defaultValue="title"
+            onChange={(event, newValue) => setSearchType(newValue)}
+          >
             <FormControlLabel value="title" control={<Radio />} label="Title" />
             <FormControlLabel
               value="author"
               control={<Radio />}
               label="Author"
             />
+            <FormControlLabel value="isbn" control={<Radio />} label="ISBN" />
             <FormControlLabel
               value="book club"
               control={<Radio />}
@@ -52,10 +94,10 @@ export default function SearchResults() {
           </RadioGroup>
         </FormControl>
       </div>
-      <div className="mt-5">
-        <h2 className="text-darkbrown">
+      <div className="col-span-4 mt-5">
+        <Typography className="text-darkbrown">
           {results.length} results for "{query}"
-        </h2>
+        </Typography>
         <ul className="flex flex-wrap gap-15">
           {results.length > 0 ? (
             results.map((book) => (
